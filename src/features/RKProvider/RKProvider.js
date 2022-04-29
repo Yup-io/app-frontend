a046ad6a95e7703ab0332d4e551351b1e5667267
Alan Martinez <alanxmartin45@gmail.com>
2022-04-29 14:34:47 -0600

    Customize RainbowKit Theme

--- src/features/RKProvider/RKProvider.js
+++ src/features/RKProvider/RKProvider.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { RainbowKitProvider, getDefaultWallets, connectorsForWallets } from '@rainbow-me/rainbowkit'
+import { useTheme } from '@mui/material'
+import { RainbowKitProvider, getDefaultWallets, connectorsForWallets, lightTheme, darkTheme } from '@rainbow-me/rainbowkit'
 import { WagmiProvider, chain } from 'wagmi'
 import { providers } from 'ethers'
+import merge from 'lodash/merge'
 
 import '@rainbow-me/rainbowkit/dist/index.css'
 
@@ -30,13 +32,27 @@ const wallets = getDefaultWallets({
 const connectors = connectorsForWallets(wallets)
 
 const RKProvider = ({ children }) => {
+  const { palette } = useTheme()
+  const rkDefaultTheme = palette.mode === 'light' ? lightTheme() : darkTheme()
+  const rkTheme = merge(rkDefaultTheme, {
+    colors: {
+      modalBackground: palette.M900
+    },
+    fonts: {
+      body: 'Gilroy'
+    }
+  })
+
   return (
     <WagmiProvider
       autoConnect
       connectors={connectors}
       provider={provider}
     >
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={rkTheme}
+      >
         {children}
       </RainbowKitProvider>
     </WagmiProvider>
--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import { SnackbarProvider } from 'notistack'
 import './styles.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import RKProvider from './features/RKProvider'
 const { NODE_ENV } = process.env
 
 let composeEnhancers
@@ -51,9 +50,7 @@ root.render(
   <Provider store={store}>
     <StylesProvider injectFirst>
       <SnackbarProvider maxSnack={3}>
-        <RKProvider>
-          <Index history={history} />
-        </RKProvider>
+        <Index history={history} />
       </SnackbarProvider>
     </StylesProvider>
   </Provider>

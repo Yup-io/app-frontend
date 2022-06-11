caccc1324d73eff66e0938c2d793b3b82869f711
Alan Martinez <alanxmartin45@gmail.com>
2022-06-10 19:38:27 -0600

    Add middleware for prerender

--- src/components/CollectionDetails/CollectionDetails.js
+++ src/components/CollectionDetails/CollectionDetails.js
@@ -8,6 +8,7 @@ import YupTabs from '../YupTabs'
 import LoadingSpin from '../LoadingSpin'
 import RecommendationList from './RecommendationList'
 import { Container } from '@mui/material'
+import YupHead from '../YupHead'
 
 const TabValues = {
   FEED: 'feed',
@@ -33,32 +34,44 @@ const CollectionDetails = ({ id }) => {
   const isTabMode = !isDesktop;
 
   return (
-    <PageLayout>
-      <CollectionHeader collection={collection} minimized={headerMinimized} />
-      {isTabMode && (
-        <Container>
-          <YupTabs
-            tabs={TabData}
-            value={currentTab}
-            onChange={(tab) => setCurrentTab(tab)}
-          />
-        </Container>
-      )}
-      {(!isTabMode || currentTab === TabValues.FEED) && (
-        <CollectionList
-          collection={collection}
-          recommendationVisible={!isTabMode}
-          onScroll={(ev) => setHeaderMinimized(ev.target.scrollTop > 0)}
-        />
-      )}
-      {isTabMode && currentTab === TabValues.RECOMMENDED && (
-        <Suspense fallback={<LoadingSpin />}>
+    <>
+      <YupHead
+        title={`${collection.name} | ${collection.owner}`}
+        description={collection.description}
+        image={collection.coverImgSrc}
+        meta={{
+          'twitter:title': `${collection.name} | ${collection.owner}`,
+          'twitter:image': collection.coverImgSrc,
+          'twitter:description': collection.description
+        }}
+      />
+      <PageLayout>
+        <CollectionHeader collection={collection} minimized={headerMinimized} />
+        {isTabMode && (
           <Container>
-            <RecommendationList collection={collection} />
+            <YupTabs
+              tabs={TabData}
+              value={currentTab}
+              onChange={(tab) => setCurrentTab(tab)}
+            />
           </Container>
-        </Suspense>
-      )}
-    </PageLayout>
+        )}
+        {(!isTabMode || currentTab === TabValues.FEED) && (
+          <CollectionList
+            collection={collection}
+            recommendationVisible={!isTabMode}
+            onScroll={(ev) => setHeaderMinimized(ev.target.scrollTop > 0)}
+          />
+        )}
+        {isTabMode && currentTab === TabValues.RECOMMENDED && (
+          <Suspense fallback={<LoadingSpin />}>
+            <Container>
+              <RecommendationList collection={collection} />
+            </Container>
+          </Suspense>
+        )}
+      </PageLayout>
+    </>
   )
 };
 
---
+++ src/pages/_middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { NextURL } from "next/dist/server/web/next-url";
+
+const PRERENDER_TOKEN = 'qMJGnydLuILOuCFcFbMS';
+const NON_SSR_REGEX = /\.(js|css|xml|less|png|jpg|jpeg|gif|pdf|doc|txt|ico|rss|zip|mp3|rar|exe|wmv|doc|avi|ppt|mpg|mpeg|tif|wav|mov|psd|ai|xls|mp4|m4a|swf|dat|dmg|iso|flv|m4v|torrent|ttf|woff|svg|eot)$/i;
+
+export function middleware(request) {
+    const { url } = request;
+    const { isBot } = request.ua;
+    const { search } = request.nextUrl;
+
+    if ((isBot || /_escaped_fragment_/.test(search)) && !NON_SSR_REGEX.test(url)) {
+        return NextResponse.redirect(
+            new NextURL(
+                `https://service.prerender.io/${url}`,
+                {
+                    headers: {
+                        'x-prerender-token': PRERENDER_TOKEN
+                    }
+                }
+            )
+        );
+    }
+}

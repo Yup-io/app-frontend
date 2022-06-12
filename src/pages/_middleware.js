95f4d950078f45534d6123127ff05f23df126c8a
Alan Martinez <alanxmartin45@gmail.com>
2022-06-12 14:23:09 -0600

    Update middleware

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,24 +1,16 @@
-import { NextResponse } from "next/server";
-import { NextURL } from "next/dist/server/web/next-url";
-
 const PRERENDER_TOKEN = 'qMJGnydLuILOuCFcFbMS';
 const NON_SSR_REGEX = /\.(js|css|xml|less|png|jpg|jpeg|gif|pdf|doc|txt|ico|rss|zip|mp3|rar|exe|wmv|doc|avi|ppt|mpg|mpeg|tif|wav|mov|psd|ai|xls|mp4|m4a|swf|dat|dmg|iso|flv|m4v|torrent|ttf|woff|svg|eot)$/i;
 
-export function middleware(request) {
-    const { url } = request;
-    const { isBot } = request.ua;
-    const { search } = request.nextUrl;
+export async function middleware(request) {
+  const { url } = request;
+  const { isBot } = request.ua;
+  const { search } = request.nextUrl;
 
-    if ((isBot || /_escaped_fragment_/.test(search)) && !NON_SSR_REGEX.test(url)) {
-        return NextResponse.redirect(
-            new NextURL(
-                `https://service.prerender.io/${url}`,
-                {
-                    headers: {
-                        'x-prerender-token': PRERENDER_TOKEN
-                    }
-                }
-            )
-        );
-    }
+  if ((isBot || /_escaped_fragment_/.test(search)) && !NON_SSR_REGEX.test(url)) {
+    return await fetch(`https://service.prerender.io/https://app.yup.io/collections/virtual-birds/DOCoGLj`, {
+      headers: {
+        'x-prerender-token': PRERENDER_TOKEN
+      }
+    });
+  }
 }

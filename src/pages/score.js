d491157584b85c9bd9c4651310632936bda37a90
daniiba <danielibanezbecker@aol.com>
2022-06-20 18:55:45 +0200

    scorepage placeholder

--- src/pages/score.js
+++ src/pages/score.js
@@ -1,7 +1,11 @@
 import ScorePage from '../_pages/ScorePage/ScorePage';
 
 const Score = () => {
-  return <ScorePage />;
+  //return <ScorePage />;
+  return (<div style={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+  <p >Not yet implemented</p>
+  <p >(╯°□°)╯︵ ┻━┻</p>
+    </div>)
 };
 
 export default Score;

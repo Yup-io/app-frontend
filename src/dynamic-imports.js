e5948f8230e2b21e836d1d77efc52375ddcddffa
Alan Martinez <alanxmartin45@gmail.com>
2022-06-20 09:19:30 -0600

    Remove Tour from dynamic imports

--- src/dynamic-imports.js
+++ src/dynamic-imports.js
@@ -1,5 +1,3 @@
 import dynamic from 'next/dynamic';
 
-export const Tour = dynamic(() => import('reactour'), { ssr: false });
-
 export const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });

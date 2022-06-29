852867fb8b5ba64db9faf86087dc9696093d6299
Alan Martinez <alanxmartin45@gmail.com>
2022-06-29 17:56:28 -0600

    Define AppUtilsContext

--- src/contexts/AppUtilsContext.js
+++ src/contexts/AppUtilsContext.js
@@ -1,9 +1,33 @@
-import { createContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AppUtilsContext = createContext({
   windowScrolled: false
 });
 
 export const AppUtilsProvider = ({ children }) => {
+  const [windowScrolled, setWindowScrolled] = useState(false);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      setWindowScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => window.removeEventListener('scroll', scrollListener);
+  }, []);
+
+  return (
+    <AppUtilsContext.Provider
+      value={{
+        windowScrolled
+      }}
+    >
+
+    </AppUtilsContext.Provider>
+  );
 };
+
+export const useAppUtils = () => useContext(AppUtilsContext);
+
+export default AppUtilsContext;

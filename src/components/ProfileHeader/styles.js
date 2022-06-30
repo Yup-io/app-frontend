b8ad2bf211f6ccda93b88c646cf6fe5c657f96e3
Alan Martinez <alanxmartin45@gmail.com>
2022-06-30 16:44:06 -0600

    Implement mobile responsivenes of header

--- src/components/ProfileHeader/styles.js
+++
@@ -1,9 +0,0 @@
-import { Avatar, styled } from '@mui/material'
-
-export const ProfilePicture = styled(Avatar)(({ theme, border }) => ({
-  backgroundColor: theme.palette.M900,
-  boxShadow: 'inset 2px 2px 0px 10px #AAAAAA10',
-  width: 100,
-  height: 100,
-  border: `solid 3px ${border}`
-}));

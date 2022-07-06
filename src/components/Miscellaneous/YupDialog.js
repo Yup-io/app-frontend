fa8f53f8a1e5e2546619a82b5b67277cf3831c65
Nicholas Fasulo <fasulo.nick@gmail.com>
2022-07-06 10:43:50 -0400

    updated YupDialog.js

--- src/components/Miscellaneous/YupDialog.js
+++ src/components/Miscellaneous/YupDialog.js
@@ -5,11 +5,12 @@ import {
   Grid,
   DialogContent,
   DialogTitle,
-  Typography,
-  Icon
+  Typography
 } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import withStyles from '@mui/styles/withStyles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/pro-light-svg-icons';
 import clsx from 'clsx';
 
 const styles = (theme) => ({
@@ -63,27 +64,27 @@ function YupDialog(props) {
   return (
     <Dialog {...restProps}>
       <DialogTitle style={{ paddingBottom: '10px' }}>
-        <Typography variant="h5">{headline}</Typography>
+        <Typography variant='h5'>{headline}</Typography>
         {restProps.onClose ? (
           <IconButton
-            aria-label="close"
+            aria-label='close'
             onClick={restProps.onClose}
             sx={{
               position: 'absolute',
               right: 8,
               top: 8,
-              color: (theme) => theme.palette.grey[500]
+              color: (theme) => theme.palette.M150
             }}
-            size="small"
+            size='medium'
           >
-            <Icon fontSize="small" className="fal fa-close" />
+            <FontAwesomeIcon icon={faXmark} />
           </IconButton>
         ) : null}
       </DialogTitle>
       <DialogContent>
-        <Grid container direction="column" spacing={2}>
+        <Grid container direction='column' spacing={2}>
           <Grid item>
-            <Typography variant="b2">{description}</Typography>
+            <Typography variant='b2'>{description}</Typography>
           </Grid>
           <Grid item>{children}</Grid>
           <Grid item>
@@ -92,7 +93,7 @@ function YupDialog(props) {
               xs={12}
               spacing={firstButton && secondButton && 2}
               direction={reverse ? 'row-reverse' : 'row'}
-              alignItems="stretch"
+              alignItems='stretch'
             >
               {firstButton && (
                 <Grid item className={clsx(full && classes.firstButton)}>

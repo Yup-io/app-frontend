e59fbc1e1fc8e2cf5beb5dbccd6da577b9a72b03
Nicholas Fasulo <fasulo.nick@gmail.com>
2022-07-14 13:27:36 -0400

    converted FollowersDialog.js to functional component

---
+++ src/components/Followers/styles.js
@@ -0,0 +1,56 @@
+import { makeStyles } from '@mui/styles'
+
+export default makeStyles((theme) => ({
+  dialogTitle: {
+    margin: 0,
+    padding: theme.spacing(1.5)
+  },
+  gridRoot: {
+    flexGrow: 1
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(),
+    top: theme.spacing(),
+    color: 'black',
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  },
+  followButton: {
+    margin: theme.spacing()
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  dialogContent: {
+    root: {
+      margin: 0,
+      padding: theme.spacing(2)
+    }
+  },
+  user: {
+    display: 'flex',
+    padding: '3% 0% 3% 0%',
+    paddingTop: '2%',
+    alignItems: 'center'
+  },
+  avatar: {
+    height: '30px',
+    paddingRight: '5%'
+  },
+  avatarImage: {
+    width: '30px',
+    height: '30px',
+    borderRadius: '50%'
+  },
+  progress: {
+    margin: theme.spacing(2),
+    color: '#ffffff'
+  },
+  Typography: {
+    fontFamily: 'Gilroy'
+  }
+}));
